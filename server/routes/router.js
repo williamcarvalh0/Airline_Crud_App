b4bccab1314630c4752cfb3957f86faa9446f185
unknown <williamcarvalh0@hotmail.com>
2021-05-01 10:10:13 +0100

    fixing functions user

--- server/routes/router.js
+++ server/routes/router.js
@@ -25,6 +25,7 @@ route.get('/book-list', services.bookList)
 // API
 route.post('/api/users', controller.create);
 route.get('/api/users', controller.find);
+route.delete('/api/users/:id', controller.delete);
  
  module.exports = route
 
