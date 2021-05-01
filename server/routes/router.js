3ead6a34750cd935cc7b1a66a7ce57302f48ba07
unknown <williamcarvalh0@hotmail.com>
2021-05-01 01:50:47 +0100

    gitignore updated

--- server/routes/router.js
+++ server/routes/router.js
@@ -25,7 +25,5 @@ route.get('/book-list', services.bookList)
 // API
 route.post('/api/users', controller.create);
 route.get('/api/users', controller.find);
-route.put('/api/users/:id', controller.update);
-route.delete('/api/users/:id', controller.delete);
  
  module.exports = route
